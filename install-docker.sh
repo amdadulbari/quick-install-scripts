2885ce957f8f19b0bcfaebd4cbf7b3c0f23b80d3
Md. Amdadul Bari Imad <amdadulbari@gmail.com>
2020-06-17 20:42:20 +0600

    docker gpg added

--- install-docker.sh
+++ install-docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt update -y
 sudo apt install -y docker-ce
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
