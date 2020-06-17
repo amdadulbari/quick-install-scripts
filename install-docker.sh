d70a5c94223c54a3b51f810ddd0d1649e3052dd8
Md. Amdadul Bari Imad <amdadulbari@gmail.com>
2020-06-17 20:27:59 +0600

    Docker & docker compose installer

---
+++ install-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
+sudo apt update -y
+sudo apt install -y docker-ce
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
